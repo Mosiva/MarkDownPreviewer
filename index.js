eafd69641e13b5652df7225b411582cebee32828
Mosiva <portret008@gmail.com>
2022-08-07 19:51:59 +0600

    add-without-css

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const App =()=>{
-    return(
-        <div className="text-center"> hello </div>
-    )
-}
+marked.setOptions({
+  breaks: true,
+});
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const renderer = new marked.Renderer();
+
+const App = () => {
+  const [text, setText] = React.useState("");
+  return (
+    <div className="text-center px-4">
+      <textarea
+        name="text"
+        id="text"
+        rows="10"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="textarea"
+      ></textarea>
+      <h3 className ="mt-3"> Output</h3>
+      <Preview markdown={text} />
+    </div>
+  );
+};
+const Preview = ({ markdown }) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: marked(markdown, { renderer: renderer }),
+      }}
+      id="preview"
+    ></div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
